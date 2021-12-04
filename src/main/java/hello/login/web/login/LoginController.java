372d6498ab15aeb357dc0d26e45e3dc8a58965a8
kangyunsik <re12io@ajou.ac.kr>
2021-12-04 16:16:52 +0900

    LoginV4 -> @RequestParam 을 이용하여 기존 필터링 되었던 Redirect 에 대한 정보를 이용할 수 있도록 구현

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -72,7 +73,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult, HttpServletRequest request){
         if(bindingResult.hasErrors()){
             log.info("Binding Error in LoginController. errors = {}",bindingResult);
@@ -92,6 +93,30 @@ public class LoginController {
         session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm loginForm, BindingResult bindingResult,
+                          @RequestParam(defaultValue="/") String redirectURL,
+                          HttpServletRequest request){
+        if(bindingResult.hasErrors()){
+            log.info("Binding Error in LoginController. errors = {}",bindingResult);
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(loginForm.getLoginId(),loginForm.getPassword());
+
+        if(loginMember == null){
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 올바르지 않습니다.");
+            return "login/loginForm";
+        }
+
+        //세션이 있다면 재사용하고, 없다면 새로 생성해서 반환
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보를 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
+
+        return "redirect:" + redirectURL;
     }
 
 //    @PostMapping("/logout")
